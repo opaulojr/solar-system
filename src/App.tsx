bb66567374cc5a919a04661c78b51b055119b6eb
opaulojr <paulojr.hs@gmail.com>
2023-11-24 13:39:00 -0300

    implement-missions-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 import './app.css';
-import SolarSystem from './components/SolarSystem';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SolarSystem />
+      <Missions />
     </div>
   );
 }
---
+++ src/components/Missions.tsx
@@ -0,0 +1,11 @@
+import Title from './Title';
+
+function Missions() {
+  return (
+    <div className="missions-component">
+      <Title headline="Missões" idName="missions-title" />
+    </div>
+  );
+}
+
+export default Missions;
