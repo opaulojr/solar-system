1b841febc436f46034dcd6f76c916291915ad12d
opaulojr <paulojr.hs@gmail.com>
2023-11-24 13:58:36 -0300

    render-missions

--- src/components/Missions.tsx
+++ src/components/Missions.tsx
@@ -1,9 +1,22 @@
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 function Missions() {
   return (
     <div className="missions-component">
       <Title headline="Missões" idName="missions-title" />
+      <section className="missions">
+        {missions.map((mission) => (
+          <MissionCard
+            key={mission.id}
+            name={mission.name}
+            year={mission.year}
+            country={mission.country}
+            destination={mission.destination}
+          />
+        ))}
+      </section>
     </div>
   );
 }
