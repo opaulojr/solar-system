4a159e0e19186e726743f8a71ffcadcfe37e16a4
opaulojr <paulojr.hs@gmail.com>
2023-11-23 22:26:55 -0300

    render-planets

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -1,9 +1,21 @@
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 function SolarSystem() {
   return (
     <div className="solar-system-component">
       <Title headline="Planetas" />
+      <hr id="planets-line" />
+      <section id="planets">
+        {planets.map((planet) => (
+          <PlanetCard
+            key={planet.id}
+            planetName={planet.name}
+            planetImage={planet.image}
+          />
+        ))}
+      </section>
     </div>
   );
 }
--- src/data/planets.ts
+++ src/data/planets.ts
@@ -8,14 +8,14 @@ import Uranoimg from '../images/Urano.png';
 import Netunoimg from '../images/Netuno.png';
 
 const Planets = [
-  { name: 'Mercúrio', image: MercurioImg },
-  { name: 'Vênus', image: VenusImg },
-  { name: 'Terra', image: TerraImg },
-  { name: 'Marte', image: MarteImg },
-  { name: 'Júpiter', image: JupiterImg },
-  { name: 'Saturno', image: SaturnoImg },
-  { name: 'Urano', image: Uranoimg },
-  { name: 'Netuno', image: Netunoimg },
+  { name: 'Mercúrio', image: MercurioImg, id: 1 },
+  { name: 'Vênus', image: VenusImg, id: 2 },
+  { name: 'Terra', image: TerraImg, id: 3 },
+  { name: 'Marte', image: MarteImg, id: 4 },
+  { name: 'Júpiter', image: JupiterImg, Id: 5 },
+  { name: 'Saturno', image: SaturnoImg, id: 6 },
+  { name: 'Urano', image: Uranoimg, id: 7 },
+  { name: 'Netuno', image: Netunoimg, id: 8 },
 ];
 
 export default Planets;
